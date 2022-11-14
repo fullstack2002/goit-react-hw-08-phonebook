53ac722b4f6bfc6b0991b3076568be4f6204553a
fullstack2002 <98652392+fullstack2002@users.noreply.github.com>
2022-11-14 20:28:00 +0200

    bugfix 3

--- src/components/routes/PrivateRoute.jsx
+++ src/components/routes/PrivateRoute.jsx
@@ -2,10 +2,8 @@ import { Navigate } from "react-router-dom";
 import { getAuth } from "redux/auth/authSlice";
 import { useSelector } from "react-redux";
 
-const PrivateRoute = ({children}) => {
+export const PrivateRoute = ({children}) => {
     const { isLoggedIn } = useSelector(getAuth)
 
     return isLoggedIn ? children : <Navigate to="/login" />
 }
-
-export default PrivateRoute;
