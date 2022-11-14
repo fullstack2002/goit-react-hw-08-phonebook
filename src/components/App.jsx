a5dc9aa2071df7416911f28624f0b88ef2abca96
fullstack2002 <98652392+fullstack2002@users.noreply.github.com>
2022-11-14 19:16:16 +0200

    routes bugfix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,8 +6,8 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { fetchCurrentUser } from "redux/auth/authOperations";
 import { getAuth } from "redux/auth/authSlice";
-import {PrivateRoute} from "./Routes/PrivateRoute";
-import {PublicRoute} from "./Routes/PublicRoute";
+import { PrivateRoute } from './Routes/PrivateRoute';
+import { PublicRoute } from './Routes/PublicRoute';
 import { Layout } from "./Phonebook/Layout/Layout";
 import { LoaderRoute } from "./Phonebook/Loader/Loader";
 
